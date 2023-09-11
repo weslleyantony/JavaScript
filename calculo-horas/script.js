130840efef290307e2eb3177834d955b204842f3
https-wesz <weslley.antony.silva@gmail.com>
2023-09-11 17:28:36 -0300

    commit javascript

---
+++ calculo-horas/script.js
@@ -0,0 +1,19 @@
+let ponto1 = document.querySelector('input#p1').value
+let ponto2 = document.querySelector('input#p2').value
+let ponto3 = document.querySelector('input#p3').value
+let ponto4 = document.querySelector('input#p4').value
+let res = document.querySelector('p#res')
+
+function horarioAlmoco(){
+    let ponto1 = document.querySelector('input#p1').value
+    let ponto2 = document.querySelector('input#p2').value
+    // let soma1 = 
+    res.innerHTML = (`<p>fia da puta ${ponto1}</p>`)
+    res.innerHTML += (`<p>fia da puta ${ponto2}</p>`)
+    res.innerHTML += (`a soma é ${soma1}`)
+}
+
+function horarioSaida(){
+    let ponto3 = document.querySelector('input#p3').value
+    let ponto4 = document.querySelector('input#p4').value
+}
